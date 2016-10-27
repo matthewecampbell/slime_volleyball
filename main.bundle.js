b7ebf96e54daa22d974e178f8737e435508a53d4
Matthew Campbell <ematthewcampbell@gmail.com>
2016-10-27 13:22:10 -0600

    forgot to run build

--- main.bundle.js
+++ main.bundle.js
@@ -48,7 +48,21 @@
 
 	__webpack_require__(10);
 
+	registerServiceWorker();
 	new Game();
+
+	function registerServiceWorker() {
+	  console.log("hello");
+	  if ('serviceWorker' in navigator) {
+	    navigator.serviceWorker.register('/lib/scripts/service_worker.js').then(function (reg) {
+	      // registration worked
+	      console.log('Registration succeeded. Scope is ' + reg.scope);
+	    }).catch(function (error) {
+	      // registration failed
+	      console.log('Registration failed with ' + error);
+	    });
+	  }
+	}
 
 /***/ },
 /* 1 */
@@ -73,7 +87,7 @@
 
 	Game.prototype.renderBackground = function (keysDown) {
 	  if (this.difficulty === "normal") {
-	    var background = { beach: "http://i.imgur.com/gDJx6ws.jpg", mars: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/PIA17944-MarsCuriosityRover-AfterCrossingDingoGapSanddune-20140209.jpg/1280px-PIA17944-MarsCuriosityRover-AfterCrossingDingoGapSanddune-20140209.jpg", underWater: "http://www.publicdomainpictures.net/pictures/140000/velka/seabed-underwater-1443611137nes.jpg", minions: "https://s-media-cache-ak0.pinimg.com/originals/59/45/06/594506d41c836f70139b90a0f67e1563.gif", lotr: "https://media.giphy.com/media/c5iRPkrIqDHQk/giphy.gif", unicorn: "http://f.owledge.de/mlp/PFUDOR_dancing_fullres.gif", mountains: "https://lonelyplanetimages.imgix.net/a/g/hi/t/12dec8938220093eb7f1fdb8a9ce40b8-the-rocky-mountains.jpg?sharp=10&vib=20&w=1200", technicolor: "http://i.imgur.com/CcKD4Jd.gif", matrix: "https://reneweddesign.files.wordpress.com/2015/07/matrix-2.gif?w=1280&h=1024&crop=1" };
+	    var background = { beach: "https://i.imgur.com/gDJx6ws.jpg", mars: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/PIA17944-MarsCuriosityRover-AfterCrossingDingoGapSanddune-20140209.jpg/1280px-PIA17944-MarsCuriosityRover-AfterCrossingDingoGapSanddune-20140209.jpg", underWater: "https://www.publicdomainpictures.net/pictures/140000/velka/seabed-underwater-1443611137nes.jpg", minions: "https://s-media-cache-ak0.pinimg.com/originals/59/45/06/594506d41c836f70139b90a0f67e1563.gif", lotr: "https://media.giphy.com/media/c5iRPkrIqDHQk/giphy.gif", mountains: "https://lonelyplanetimages.imgix.net/a/g/hi/t/12dec8938220093eb7f1fdb8a9ce40b8-the-rocky-mountains.jpg?sharp=10&vib=20&w=1200", technicolor: "https://i.imgur.com/CcKD4Jd.gif", matrix: "https://reneweddesign.files.wordpress.com/2015/07/matrix-2.gif?w=1280&h=1024&crop=1" };
 	    for (var key in keysDown) {
 	      var value = Number(key);
 	      if (value === 49) {
@@ -90,9 +104,6 @@
 	      }
 	      if (value === 53) {
 	        this.changeBackgroundImage(background.lotr);
-	      }
-	      if (value === 54) {
-	        this.changeBackgroundImage(background.unicorn);
 	      }
 	      if (value === 55) {
 	        this.changeBackgroundImage(background.mountains);
@@ -1360,7 +1371,7 @@
 
 
 	// module
-	exports.push([module.id, "canvas {\n    padding-left: 0;\n    padding-right: 0;\n    margin-left: auto;\n    margin-right: auto;\n    display: block;\n    background: url(\"http://i.imgur.com/gDJx6ws.jpg\") no-repeat;\n    background-position: center;\n}\n", ""]);
+	exports.push([module.id, "canvas {\n    padding-left: 0;\n    padding-right: 0;\n    margin-left: auto;\n    margin-right: auto;\n    display: block;\n    background: blue no-repeat;\n    background-position: center;\n}\n", ""]);
 
 	// exports
 
